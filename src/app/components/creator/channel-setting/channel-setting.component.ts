50077f7e43d89ae0689477f11698161956202a3e
mkcodelab <michal.kulawik@gmail.com>
2024-05-10 11:20:06 +0200

    preset creation added

---
+++ src/app/components/creator/channel-setting/channel-setting.component.ts
@@ -0,0 +1,31 @@
+import { NgFor } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Waveform } from '../../../services/synth.service';
+import { OscillatorData } from '../../../services/preset-creator.service';
+
+export interface ChannelData extends OscillatorData {
+  channel: 'left' | 'right';
+}
+
+@Component({
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './channel-setting.html',
+  selector: 'channel-setting',
+})
+export class ChannelSettingComponent {
+  waveFormOptions: Waveform[] = ['sine', 'sawtooth', 'triangle', 'square'];
+  @Input({ required: true }) channel: 'left' | 'right';
+
+  @Output() oscillatorData = new EventEmitter<ChannelData>();
+
+  sendOscillatorData(type: string, frequency: number) {
+    let waveformType = type as Waveform;
+
+    this.oscillatorData.emit({
+      type: waveformType,
+      frequency: frequency,
+      channel: this.channel,
+    });
+  }
+}
--- src/app/components/creator/creator.component.ts
+++ src/app/components/creator/creator.component.ts
@@ -1,24 +1,64 @@
 import { Component, inject } from '@angular/core';
 import {
+  OscillatorData,
   Preset,
   PresetCreatorService,
 } from '../../services/preset-creator.service';
+import { Waveform } from '../../services/synth.service';
+import { NgFor } from '@angular/common';
+import {
+  ChannelData,
+  ChannelSettingComponent,
+} from './channel-setting/channel-setting.component';
 
 @Component({
   standalone: true,
   selector: 'creator',
   templateUrl: './creator.html',
-  imports: [],
+  imports: [NgFor, ChannelSettingComponent],
 })
 export class CreatorComponent {
   private presetService = inject(PresetCreatorService);
 
-  addPreset(preset?: Preset) {
-    // this.presetService.addPreset(preset);
+  leftChannelOscillators: OscillatorData[] = [];
+  rightChannelOscillators: OscillatorData[] = [];
+
+  presetTitle: string;
+
+  //   waveFormOptions: Waveform[] = ['sine', 'sawtooth', 'triangle', 'square'];
+
+  addPreset() {
+    const preset = this.createPreset();
+    this.presetService.addPreset(preset);
     this.displayMessage();
   }
 
   displayMessage() {
     alert('preset added!');
+  }
+
+  createPreset(): Preset {
+    let preset = new Preset(this.presetTitle);
+    for (let osc of this.leftChannelOscillators) {
+      preset.addOscillator('left', osc);
+    }
+    for (let osc of this.rightChannelOscillators) {
+      preset.addOscillator('right', osc);
+    }
+    return preset;
+  }
+
+  recieveOscillatorData(data: ChannelData) {
+    console.log(data);
+    this.addOscillatorData(data.channel, {
+      type: data.type,
+      frequency: data.frequency,
+    });
+  }
+
+  addOscillatorData(channel: 'left' | 'right', data: OscillatorData) {
+    channel === 'left'
+      ? this.leftChannelOscillators.push(data)
+      : this.rightChannelOscillators.push(data);
   }
 }
