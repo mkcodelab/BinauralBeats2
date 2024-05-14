221f320fe4725ec3d7370d084ffc24464304be0b
mkcodelab <michal.kulawik@gmail.com>
2024-05-14 14:53:40 +0200

    styling pt2

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 export interface NavItem {
   name: string;
@@ -9,15 +9,16 @@ export interface NavItem {
 
 @Component({
   standalone: true,
-  imports: [RouterLink, NgFor, NgClass],
+  imports: [RouterLink, RouterLinkActive, NgFor, NgClass],
   selector: 'navbar',
   template: `
     <div class="p-2 bg-slate-200/20 flex justify-center">
       <button
-        class="p-2 bg-slate-500 text-white rounded-xl"
+        class="p-2 border border-white bg-slate-200/20 text-white rounded-xl"
         [ngClass]="{ 'mr-2': !isLast }"
         *ngFor="let item of navItems; last as isLast"
         [routerLink]="item.url"
+        [routerLinkActive]="'gradient-bg'"
       >
         {{ item.name }}
       </button>
